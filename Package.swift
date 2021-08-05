35a413bd6816321e4a02d5b2a8295a7e654134e1
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-08-05 14:44:41 +0500

    Add `ArgumentParser` as a dependency

--- Package.swift
+++ Package.swift
@@ -5,8 +5,9 @@ import PackageDescription
 let package = Package(
     name: "chaqmoq-console",
     products: [.library(name: "Console", targets: ["Console"])],
+    dependencies: [.package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.4")],
     targets: [
-        .target(name: "Console"),
+        .target(name: "Console", dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]),
         .testTarget(name: "ConsoleTests", dependencies: [.target(name: "Console")])
     ],
     swiftLanguageVersions: [.v5]
