6e762c2cc56e88904f9914b70e19b5c2700cd9b0
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-11-06 16:06:35 +0500

    Fix formatting

--- Package.swift
+++ Package.swift
@@ -4,10 +4,16 @@ import PackageDescription
 
 let package = Package(
     name: "chaqmoq-console",
-    products: [.library(name: "Console", targets: ["Console"])],
-    dependencies: [.package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.4")],
+    products: [
+        .library(name: "Console", targets: ["Console"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.4")
+    ],
     targets: [
-        .target(name: "Console", dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")])
+        .target(name: "Console", dependencies: [
+            .product(name: "ArgumentParser", package: "swift-argument-parser")
+        ])
     ],
     swiftLanguageVersions: [.v5]
 )
